703341f0201477f531f90f68e0c536872941e9dc
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-01-23 12:26:40 +0000

    add task

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""
-8-make_multiplier.py
-"""
+"""8-make_multiplier.py"""
+
+from typing import Callable
 
 
-def make_multiplier(m: float) -> callable:
-    """make_multiplier: takes a float m as argument
-    and returns a function that multiplies a float by m."""
-    def multiplier(x: float) -> float:
-        return x * m
-    return multiplier
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """make_multiplier function"""
+    def reterned_function(number: float) -> float:
+        """reterned_function function"""
+        return number * multiplier
+    return reterned_function
