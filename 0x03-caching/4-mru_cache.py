1231f1f8ce01aff7dbf4b52a51a442bc9e0b353c
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-01-27 12:25:06 +0000

    add task

--- 0x03-caching/4-mru_cache.py
+++ 0x03-caching/4-mru_cache.py
@@ -1,32 +1,35 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """MRUCache module"""
-from base_caching import BaseCaching
+
+from collections import OrderedDict
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """MRUCache class"""
+    """MRUCache"""
 
     def __init__(self):
-        """Constructor"""
         super().__init__()
-        self.cache_list = []
+        self.mru_order = OrderedDict()
 
     def put(self, key, item):
         """Assign to the dictionary the item value for the key"""
-        if key in self.cache_list:
-            self.cache_list.remove(key)
-        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-            MostRecentlyUsedKey = self.cache_list.pop()
-            del self.cache_data[MostRecentlyUsedKey]
-            print("DISCARD: {}".format(MostRecentlyUsedKey))
-        if key and item:
-            self.cache_list.insert(0, key)
-            self.cache_data[key] = item
+        if not key or not item:
+            return
+        self.cache_data[key] = item
+        self.mru_order[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            item_discarded = next(iter(self.mru_order))
+            del self.cache_data[item_discarded]
+            print("DISCARD:", item_discarded)
+        if len(self.mru_order) > BaseCaching.MAX_ITEMS:
+            self.mru_order.popitem(last=False)
+        self.mru_order.move_to_end(key, False)
 
     def get(self, key):
         """Return the value in self.cache_data linked to key"""
-        if key and key in self.cache_data:
-            self.cache_list.remove(key)
-            self.cache_list.insert(0, key)
+        if key in self.cache_data:
+            self.mru_order.move_to_end(key, False)
             return self.cache_data[key]
         return None
