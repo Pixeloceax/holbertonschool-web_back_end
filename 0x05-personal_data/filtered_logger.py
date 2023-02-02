438375bb5882ba80d908548ad98cd606a883eaf1
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-02-02 11:35:59 +0000

    add comment

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -9,18 +9,25 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """
+    Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields=None):
+        """
+        Constructor
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields or []
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record
+        """
         for field in self.fields:
             record.msg = re.sub(
                 f"{field}=\S+?;", f"{field}={self.REDACTION};", record.msg)
@@ -28,6 +35,9 @@ class RedactingFormatter(logging.Formatter):
 
 
 def get_logger() -> logging.Logger:
+    """
+    Returns a logging.Logger object
+    """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
@@ -38,6 +48,9 @@ def get_logger() -> logging.Logger:
 
 
 def get_db():
+    """
+    Connect to the database
+    """
 
     username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
