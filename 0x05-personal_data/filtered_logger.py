095e6426368e370080c26589ce5031b72f7cdfa9
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-02-02 12:00:46 +0000

    fix def init

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -19,12 +19,12 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields=None):
+    def __init__(self, fields: List[str]):
         """
         Constructor
         """
+        self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields or []
 
     def format(self, record: logging.LogRecord) -> str:
         """
