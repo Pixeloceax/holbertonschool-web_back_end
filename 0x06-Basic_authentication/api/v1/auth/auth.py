fb805986d8c7a620efef98c6c6e97080dbc764ee
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-02-07 09:33:40 +0000

    fix

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Auth"""
+"""Auth"""
 
 from flask import request
 from typing import List, TypeVar
@@ -10,20 +10,20 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth
         """
-        if path is None or excluded_paths is None or\
-                not isinstance(excluded_paths, list):
-            return(True)
-        if path[-1] != '/':
-            path += '/'
-        if path in excluded_paths:
-            return(False)
-        for excluded_path in excluded_paths:
-            if excluded_path[-1] != '/':
-                excluded_path += '/'
-            if excluded_path.endswith('*'):
-                if path.startswith(excluded_path[:-1]):
-                    return(False)
-        return(True)
+        if not path or not excluded_paths:
+            return True
+        path += '/' if path[-1] != '/' else ''
+        all_path = any(rex.endswith("*") for rex in excluded_paths)
+        if not all_path:
+            if path in excluded_paths:
+                return False
+        for rex in excluded_paths:
+            if rex[-1] == '*':
+                if path.startswith(rex[:-1]):
+                    return False
+            if rex == path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header
