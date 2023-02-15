67eec72bc84be8d68a3161f9602ee877dbd0527f
Axel <axel.valentinbvs@yahoo.com>
2023-02-15 13:15:53 +0000

    add raise valueerror

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -55,5 +55,6 @@ class DB:
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
             if hasattr(user, key):
-                setattr(user, key, value)
+                raise ValueError
+            setattr(user, key, value)
         self._session.commit()
