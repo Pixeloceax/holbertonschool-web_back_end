2f3e6f2d37d9d684717565cbece3f86691850b45
Axel <axel.valentinbvs@yahoo.com>
2023-02-15 14:21:30 +0000

    test with Union from typing

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ import bcrypt
 from db import DB
 from user import User
 from uuid import uuid4
+from sqlalchemy.orm.exc import NoResultFound
 from typing import Union
 
 
@@ -79,7 +80,7 @@ class Auth:
         try:
             user = self._db.find_user_by(session_id=session_id)
             return user
-        except Exception:
+        except NoResultFound:
             return None
 
     def destroy_session(self, user_id: int) -> None:
