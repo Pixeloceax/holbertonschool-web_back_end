b0a0eab7d4701596079df0ffc12a2c23c1e66bb6
Axel <axel.valentinbvs@yahoo.com>
2023-02-16 09:28:56 +0000

    test

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -75,8 +75,7 @@ class Auth:
         if session_id is None:
             return None
         try:
-            user = self._db.find_user_by(session_id=session_id)
-            return user
+            return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
 
