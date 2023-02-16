38fb93fcac6897e4742a103e3431ada4c74d63a4
Axel <axel.valentinbvs@yahoo.com>
2023-02-16 09:14:12 +0000

    test

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -68,14 +68,14 @@ class Auth:
         except Exception:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> User:
-        """
-        Get the corresponding user for a session ID
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ Get user from session ID
         """
         if session_id is None:
             return None
         try:
-            return self._db.find_user_by(session_id=session_id)
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
 
