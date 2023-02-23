4e159ebf4a9de1e4d0e71f6329ade0cfc7806258
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-02-23 12:40:19 +0000

    add task

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,40 +1,46 @@
 #!/usr/bin/env python3
 """
-app module
+Flask app
 """
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
+
 app = Flask(__name__)
-babel = Babel()
 
 
-class Config(object):
+class Config():
     """
-    Config class
+    class config
     """
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
-Babel.default_locale = 'en'
-Babel.default_timezone = 'UTC'
+babel = Babel(app)
+babel.init_app(app)
+
+
+@app.route('/')
+def index():
+    """
+    index route
+    """
+
+    title = "Welcome to Holberton"
+    h1 = "Hello world"
+    return render_template("2-index.html", title=title, hello=h1)
 
 
 @babel.localeselector
 def get_locale():
     """
-    get_locale function
+    define the best language
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/", methods=["GET"])
-def index():
-    """
-    index route
-    """
-    return render_template("3-index.html")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
