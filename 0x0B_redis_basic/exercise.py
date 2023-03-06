22c93fd4841bd511cffb4ad2f1c8ca94eb0672be
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-03-06 12:59:10 +0000

    Add 0x0B_redis_basic files and initial code

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+import redis
+import uuid
+from functools import wraps
+import ast
+
+
+def count_calls(method: callable) -> callable:
+    """
+    Count calls
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(*args, **kwds):
+        """
+        Wrapper
+        """
+        args[0]._redis.incr(key)
+        return method(*args, **kwds)
+
+    return wrapper
+
+
+def call_history(method: callable) -> callable:
+    """
+    Call history
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(*args, **kwds):
+        """
+        Wrapper
+        """
+        args[0]._redis.rpush("{}:inputs".format(key), str(args[1:]))
+        result = method(*args, **kwds)
+        args[0]._redis.rpush("{}:outputs".format(key), str(result))
+        return result
+
+    return wrapper
+
+
+def replay(method: callable) -> None:
+    """
+    Replay function that shows the history of calls of a particular function
+    """
+    inputs_key = method.__qualname__ + ":inputs"
+    outputs_key = method.__qualname__ + ":outputs"
+    inputs = Cache._redis.lrange(inputs_key, 0, -1)
+    outputs = Cache._redis.lrange(outputs_key, 0, -1)
+    count = len(inputs)
+    print(f"{method.__qualname__} was called {count} times:")
+    for i, (input_str, output_str) in enumerate(zip(inputs, outputs)):
+        input_repr = ", ".join(ast.literal_eval(input_str.decode()))
+        output_repr = output_str.decode()
+        print(f"{method.__qualname__}(*({input_repr},)) -> {output_repr}")
+
+
+class Cache:
+    """
+    Cache class
+    """
+
+    def __init__(self) -> None:
+        """
+        Constructor
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @count_calls
+    @call_history
+    def store(self, data) -> str:
+        """
+        Store data in redis
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn=None) -> bytes:
+        """
+        Get data from redis
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Get data from redis as string
+        """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """
+        Get data from redis as int
+        """
+        return self.get(key, int)
