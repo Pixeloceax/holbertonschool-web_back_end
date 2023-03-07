59b2a7868e6afff65881996f70cf918722459ba4
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-03-07 09:38:39 +0000

    Add caching functionality with Redis to web.py

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python3
 """
-Web module
+web.py
 """
-
+import redis
 import requests
+from typing import Callable
+from functools import wraps
+
+redis = redis.Redis()
 
 
+def wrap_requests(fn: Callable) -> Callable:
+    """
+    wrap_requests method
+    """
+
+    @wraps(fn)
+    def wrapper(url):
+        """
+        Wrapper method
+        """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
+
+    return wrapper
+
+
+@wrap_requests
 def get_page(url: str) -> str:
     """
-    Get page
+    Get page method
     """
-    r = requests.get(url)
-    return r.text
+    response = requests.get(url)
+    return response.text
