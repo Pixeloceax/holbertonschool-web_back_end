561783b60e135b0a6ab9552a2084e7c4e4512f3e
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-03-08 12:50:17 +0000

    Add trigger Mytrigger to items table for updating quantity in products table when inserting/updating items

---
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE TRIGGER Mytrigger
+BEFORE INSERT OR UPDATE ON items
+FOR EACH ROW
+EGIN
+  UPDATE products
+       SET quantity = quantity-New.quantity
+        WHERE product_code=New.product_code;
+END;
