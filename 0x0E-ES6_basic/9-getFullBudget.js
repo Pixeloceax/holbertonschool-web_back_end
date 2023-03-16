fb255cc193dbfdeaafe038fd27ff5d2ef299ab9d
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-03-16 09:36:46 +0000

    Refactor getIncomeInDollars() and getIncomeInEuros() methods to accept income param

--- 0x0E-ES6_basic/9-getFullBudget.js
+++ 0x0E-ES6_basic/9-getFullBudget.js
@@ -1,14 +1,14 @@
-import getBudgetObject from "./7-getBudgetObject.js";
+import getBudgetObject from "./7-getBudgetObject";
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars() {
-      return `'$'${income}`;
+    getIncomeInDollars(income) {
+      return `$${income}`;
     },
-    getIncomeInEuros() {
-      return `${income} 'euros'`;
+    getIncomeInEuros(income) {
+      return `${income} euros`;
     },
   };
 
