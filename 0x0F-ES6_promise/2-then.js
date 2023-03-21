8894a710a8eaa4b1bb59ffd42d8214c1675990f2
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-03-21 09:34:41 +0000

    Update single and double quotes in handleResponseFromAPI()

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then(() => {
-      console.log("Got a response from the API");
+      console.log('Got a response from the API');
       return {
         status: 200,
-        body: "success",
+        body: 'success',
       };
     })
     .catch(() => {
-      console.log("Got an error from the API");
+      console.log('Got an error from the API');
       return new Error();
     });
 }
