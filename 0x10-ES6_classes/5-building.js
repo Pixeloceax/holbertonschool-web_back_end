35d3c526fd7db6700c353302622b05a0872f6a29
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-03-22 23:47:39 +0000

    Refactor sqft validation check

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -8,7 +8,9 @@ export default class Building {
         'Class extending Building must override evacuationWarningMessage'
       );
     }
-
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
     this._sqft = sqft;
   }
 
