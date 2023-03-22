d58d8dc581619c7461e19fb829d6f7287b141417
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-03-22 23:45:30 +0000

    Add check to ensure classes extending Building override evacuationWarningMessage

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -2,19 +2,17 @@ export default class Building {
   constructor(sqft) {
     if (
       this.constructor !== Building &&
-      typeof this.evacuationWarningMessage === 'undefined'
-    )
+      typeof this.evacuationWarningMessage !== 'function'
+    ) {
       throw new Error(
         'Class extending Building must override evacuationWarningMessage'
       );
+    }
+
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(value) {
-    this._sqft = value;
   }
 }
