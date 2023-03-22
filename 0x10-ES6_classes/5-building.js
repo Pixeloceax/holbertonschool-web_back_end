e95a0c6f251eda288718e6e717cde16b275da3e9
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-03-22 23:39:33 +0000

    Replace undefined check with typeof check for evacuationWarningMessage

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -2,12 +2,13 @@ export default class Building {
   constructor(sqft = 0) {
     if (
       this.constructor !== Building &&
-      this.evacuationWarningMessage === undefined
+      typeof this.evacuationWarningMessage !== 'function'
     ) {
       throw new Error(
         'Class extending Building must override evacuationWarningMessage'
       );
     }
+
     this._sqft = sqft;
   }
 
