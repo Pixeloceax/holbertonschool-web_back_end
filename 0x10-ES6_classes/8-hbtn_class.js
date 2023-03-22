c441c43907c5d152bf7cf024ad8e33dbc37247da
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-03-22 23:55:16 +0000

    Update constructor parameters to require size and location

--- 0x10-ES6_classes/8-hbtn_class.js
+++ 0x10-ES6_classes/8-hbtn_class.js
@@ -1,23 +1,7 @@
 export default class HolbertonClass {
-  constructor(size = 0, location = '') {
+  constructor(size, location) {
     this._size = size;
     this._location = location;
-  }
-
-  get size() {
-    return this._size;
-  }
-
-  set size(newSize) {
-    this._size = newSize;
-  }
-
-  get location() {
-    return this._location;
-  }
-
-  set location(newLocation) {
-    this._location = newLocation;
   }
 
   [Symbol.toPrimitive](value) {
