b204264fa312e77170d84b91939bc2e7401c1421
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-03-22 23:42:08 +0000

    Add setter for sqft in Building class and enforce evacuationWarningMessage override

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,18 +1,20 @@
 export default class Building {
-  constructor(sqft = 0) {
+  constructor(sqft) {
     if (
       this.constructor !== Building &&
-      typeof this.evacuationWarningMessage !== 'function'
-    ) {
+      typeof this.evacuationWarningMessage === 'undefined'
+    )
       throw new Error(
         'Class extending Building must override evacuationWarningMessage'
       );
-    }
-
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
   }
 }
