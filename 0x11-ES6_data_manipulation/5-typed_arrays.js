b5c73e81bdffccd552a0876b718c33b0a9396edb
Axel <axel.valentinbvs@yahoo.com>
2023-03-28 09:47:18 +0000

    "Add createInt8TypedArray function with improved performance and error handling using typed arrays and DataView."

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
+  if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
+  const int8 = new Int8Array(length);
+  int8[position] = value;
 
-  const arrayBuffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(arrayBuffer);
-
-  int8Array[position] = value;
-
-  return arrayBuffer;
+  const { arraybuffer } = int8;
+  const result = new DataView(arraybuffer, 0, length);
+  return result;
 }
