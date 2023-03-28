a700047d4f45403f2c4afe52921a930d46355d35
Axel <axel.valentinbvs@yahoo.com>
2023-03-28 09:43:17 +0000

    Update student grades by city using ES6 arrow functions and array methods

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,18 @@
-export default function updateStudentGradeByCity(Students, City, newGrades) {
-  const filteredCity = Students.filter((student) => student.location === City);
-  const filteredIds = filteredCity.map((student) => student.id);
-  const filteredGrades = newGrades.filter((grade) =>
-    filteredIds.includes(grade.studentId)
-  );
-  const finalGrades = filteredCity.map((student) => {
-    const grade = filteredGrades.find(
-      (grade) => grade.studentId === student.id
-    );
-    if (grade) {
-      student.grade = grade.grade;
-    } else {
-      student.grade = 'N/A';
-    }
-    return student;
-  });
-  return finalGrades;
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find(
+        (grade) => grade.studentId === student.id
+      );
+
+      const updateStudent = student;
+      if (gradeObj) {
+        updateStudent.grade = gradeObj.grade;
+      } else {
+        updateStudent.grade = 'N/A';
+      }
+
+      return updateStudent;
+    });
 }
