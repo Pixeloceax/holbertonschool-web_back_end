701c76c16009d9e1cd376c850e28186fb71e48b2
Axel <axel.valentinbvs@yahoo.com>
2023-03-29 09:43:35 +0000

    Add files and function to count students by field and print results to console

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function countStudents(CsvFile) {
+  try {
+    const data = fs.readFileSync(CsvFile, 'utf8');
+    const lines = data.trim().split('\n');
+    const studentsByField = {};
+    for (let i = 1; i < lines.length; i++) {
+      const [name, age, location, field] = lines[i].split(',');
+      if (studentsByField[field]) {
+        studentsByField[field].push(name);
+      } else {
+        studentsByField[field] = [name];
+      }
+    }
+    console.log(`Number of students: ${lines.length - 1}`);
+    for (const field in studentsByField) {
+      const students = studentsByField[field];
+      console.log(
+        `Number of students in ${field}: ${
+          students.length
+        }. List: ${students.join(', ')}`
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
