b60856f0e625b1134efb5d64f50619fe1b8a4965
Axel <axel.valentinbvs@yahoo.com>
2023-03-30 09:15:18 +0000

    Add Node.js HTTP server with async file reading capabilities to handle GET requests in 5-http.js

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const countStudents = require('./3-read_file_async');
+
+const server = http.createServer(async (req, res) => {
+  if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n\n');
+    try {
+      const data = await countStudents('database.csv');
+      res.end(data);
+    } catch (err) {
+      res.end('Cannot load the database');
+    }
+  } else if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+server.listen(1245, () => {
+  console.log('Server running at http://localhost:1245/');
+});
