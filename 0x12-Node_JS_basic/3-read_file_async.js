c214087d3c133fefcd21be981ac6a5a3a25aa1ba
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-04-03 09:27:07 +0000

    3

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 
 async function countStudents(CsvFile) {
   try {
-    const data = await fs.promises.readFile(CsvFile, 'utf8');
+    const data = await fs.readFileSync(CsvFile, 'utf8');
     const lines = data.trim().split('\n');
     const studentsByField = {};
-    for (let i = 1; i < lines.length; i++) {
-      const [name, age, location, field] = lines[i].split(',');
-      if (studentsByField[field]) {
+    for (let i = 1; i < lines.length; i += 1) {
+      const [name, , , field] = lines[i].split(',');
+      if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
         studentsByField[field].push(name);
       } else {
         studentsByField[field] = [name];
@@ -15,16 +15,16 @@ async function countStudents(CsvFile) {
     }
     console.log(`Number of students: ${lines.length - 1}`);
     for (const field in studentsByField) {
-      const students = studentsByField[field];
-      console.log(
-        `Number of students in ${field}: ${
-          students.length
-        }. List: ${students.join(', ')}`,
-      );
+      if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
+        const students = studentsByField[field];
+        console.log(
+          `Number of students in ${field}: ${
+            students.length
+          }. List: ${students.join(', ')}`
+        );
+      }
     }
   } catch (err) {
-    throw new Error('Cannot load the database');
+    throw new Error(`Cannot load the database: ${err}`);
   }
 }
-
-module.exports = countStudents;
