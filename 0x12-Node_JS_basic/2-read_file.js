06b09ff82818b7fcbe3ff120616c78775538f9ed
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-04-03 09:23:35 +0000

    2

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -5,9 +5,9 @@ function countStudents(CsvFile) {
     const data = fs.readFileSync(CsvFile, 'utf8');
     const lines = data.trim().split('\n');
     const studentsByField = {};
-    for (let i = 1; i < lines.length; i++) {
-      const [name, age, location, field] = lines[i].split(',');
-      if (studentsByField[field]) {
+    for (let i = 1; i < lines.length; i += 1) {
+      const [name, , , field] = lines[i].split(',');
+      if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
         studentsByField[field].push(name);
       } else {
         studentsByField[field] = [name];
@@ -15,15 +15,17 @@ function countStudents(CsvFile) {
     }
     console.log(`Number of students: ${lines.length - 1}`);
     for (const field in studentsByField) {
-      const students = studentsByField[field];
-      console.log(
-        `Number of students in ${field}: ${
-          students.length
-        }. List: ${students.join(', ')}`,
-      );
+      if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
+        const students = studentsByField[field];
+        console.log(
+          `Number of students in ${field}: ${
+            students.length
+          }. List: ${students.join(', ')}`,
+        );
+      }
     }
   } catch (err) {
-    throw new Error('Cannot load the database');
+    throw new Error(`Cannot load the database: ${err}`);
   }
 }
 
