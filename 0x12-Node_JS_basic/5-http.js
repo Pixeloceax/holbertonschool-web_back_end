f2cd54314f421c4dfa75f61f42b7f57efe2da3a7
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-04-03 09:44:39 +0000

    refac

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,25 +1,22 @@
 const http = require('http');
+
+const host = 'localhost';
+const port = 1245;
+
 const countStudents = require('./3-read_file_async');
 
 const server = http.createServer(async (req, res) => {
-  if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('This is the list of our students\n\n');
-    try {
-      const data = await countStudents('database.csv');
-      res.end(data);
-    } catch (err) {
-      res.end('Cannot load the database');
-    }
-  } else if (req.url === '/') {
+  if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
+  }
+
+  if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(await countStudents(process.argv[2]));
   }
 });
 
-server.listen(1245, () => {
-  console.log('Server running at http://localhost:1245/');
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
 });
