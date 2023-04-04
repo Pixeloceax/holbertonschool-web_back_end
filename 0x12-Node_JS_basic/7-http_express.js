1966ce88e18f2b091ea158ee610da813d6e8e85a
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-04-04 09:25:00 +0000

    refac

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -2,30 +2,26 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
+const filePath = process.argv[2];
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
+
 app.get('/students', (req, res) => {
-  countStudents(process.argv[2])
+  res.write('This is the list of our students\n');
+  countStudents(filePath)
     .then((data) => {
-      let num = 0;
-      let response = '';
-      for (const [key, value] of Object.entries(data)) {
-        num += value.length;
-        response += `Number of students in ${key}: ${
-          value.length
-        }. List: ${value.join(', ')}\n`;
-      }
-      response = `This is the list of our students\nNumber of students: ${num}\n${response.slice(
-        0,
-        -1,
-      )}`;
-      res.send(response);
+      res.write(data.join('\n'));
+      res.end();
     })
-    .catch((error) => {
-      res.send(`This is the list of our students\n${error.message}`);
+    .catch(() => {
+      res.end('Cannot load the database');
     });
 });
-app.listen(1245);
+
+app.listen(1245, () => {
+  console.log('Listening on port 1245');
+});
+
 module.exports = app;
