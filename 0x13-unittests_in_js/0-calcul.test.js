2d382e0049894715db56c0717ac6175af69c1aaf
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-04-05 11:12:13 +0000

    add test

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,76 +1,180 @@
 const assert = require("assert");
 const calculateNumber = require("./0-calcul");
 
-describe("calculateNumber", () => {
-  it("should return the sum of two rounded numbers", () => {
-    const result = calculateNumber(2.4, 3.5);
-    assert.strictEqual(result, 6);
+describe("calculateNumber", function () {
+  it("should return 4 when passed 1 and 3", function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it("should return the sum of a positive rounded number and a negative rounded number", () => {
-    const result = calculateNumber(2.4, -3.5);
-    assert.strictEqual(result, -1);
+  it("should return 4 when passed 1 and 3.4", function () {
+    assert.strictEqual(calculateNumber(1, 3.4), 4);
   });
 
-  it("should return the sum of two whole numbers", () => {
-    const result = calculateNumber(2, 3);
-    assert.strictEqual(result, 5);
+  it("should return 5 when passed 1 and 3.6", function () {
+    assert.strictEqual(calculateNumber(1, 3.6), 5);
   });
 
-  it("should return the sum of two negative rounded numbers", function () {
-    assert.strictEqual(calculateNumber(-3.7, -2.2), -6);
+  it("should return 4 when passed 1.4 and 3", function () {
+    assert.strictEqual(calculateNumber(1.4, 3), 4);
   });
 
-  it("should return the sum of two negative whole numbers", function () {
-    assert.strictEqual(calculateNumber(-3, -2), -5);
+  it("should return 5 when passed 1.6 and 3", function () {
+    assert.strictEqual(calculateNumber(1.6, 3), 5);
   });
 
-  it("should return the sum of two positive numbers", function () {
-    assert.strictEqual(calculateNumber(2, 3), 5);
+  it("should return 4 when passed 1.4 and 3.4", function () {
+    assert.strictEqual(calculateNumber(1.4, 3.4), 4);
   });
 
-  it("should return the sum of a positive and a negative number", function () {
-    assert.strictEqual(calculateNumber(2, -3), -1);
+  it("should return 5 when passed 1.4 and 3.6", function () {
+    assert.strictEqual(calculateNumber(1.4, 3.6), 5);
   });
 
-  it("should return the sum of two negative numbers", function () {
-    assert.strictEqual(calculateNumber(-2, -3), -5);
+  it("should return 5 when passed 1.6 and 3.4", function () {
+    assert.strictEqual(calculateNumber(1.6, 3.4), 5);
   });
 
-  it("should return NaN if one of the inputs is not a number", function () {
-    assert(isNaN(calculateNumber("foo", 2)));
-    assert(isNaN(calculateNumber(2, "bar")));
+  it("should return 6 when passed 1.6 and 3.6", function () {
+    assert.strictEqual(calculateNumber(1.6, 3.6), 6);
   });
 
-  it("should return NaN if both inputs are not numbers", () => {
-    assert(isNaN(calculateNumber("foo", "bar")));
+  it("should return 2 when passed -1 and 3", function () {
+    assert.strictEqual(calculateNumber(-1, 3), 2);
   });
 
-  it("should return Infinity if one of the inputs is Infinity", () => {
-    assert.strictEqual(calculateNumber(Infinity, 2), Infinity);
-    assert.strictEqual(calculateNumber(2, Infinity), Infinity);
+  it("should return -2 when passed 1 and -3", function () {
+    assert.strictEqual(calculateNumber(1, -3), -2);
   });
 
-  it("should return -Infinity if one of the inputs is -Infinity", () => {
-    assert.strictEqual(calculateNumber(-Infinity, 2), -Infinity);
-    assert.strictEqual(calculateNumber(2, -Infinity), -Infinity);
+  it("should return 2 when passed -1.4 and 3", function () {
+    assert.strictEqual(calculateNumber(-1.4, 3), 2);
   });
 
-  it("should return NaN if one of the inputs is NaN", () => {
-    assert(isNaN(calculateNumber(NaN, 2)));
-    assert(isNaN(calculateNumber(2, NaN)));
+  it("should return 1 when passed -1.6 and 3", function () {
+    assert.strictEqual(calculateNumber(-1.6, 3), 1);
   });
 
-  it("should return NaN if both inputs are NaN", () => {
+  it("should return -2 when passed 1 and -3.4", function () {
+    assert.strictEqual(calculateNumber(1, -3.4), -2);
+  });
+
+  it("should return -3 when passed 1 and -3.6", function () {
+    assert.strictEqual(calculateNumber(1, -3.6), -3);
+  });
+
+  it("should return -2 when passed 1.4 and -3.4", function () {
+    assert.strictEqual(calculateNumber(1.4, -3.4), -2);
+  });
+
+  it("should return -1 when passed 1.6 and -3.4", function () {
+    assert.strictEqual(calculateNumber(1.6, -3.4), -1);
+  });
+
+  it("should return -3 when passed 1.4 and -3.6", function () {
+    assert.strictEqual(calculateNumber(1.4, -3.6), -3);
+  });
+
+  it("should return -2 when passed 1.6 and -3.6", function () {
+    assert.strictEqual(calculateNumber(1.6, -3.6), -2);
+  });
+
+  it("should return 2 when passed -1.4 and 3.4", function () {
+    assert.strictEqual(calculateNumber(-1.4, 3.4), 2);
+  });
+
+  it("should return 2 when passed -1.6 and 3.4", function () {
+    assert.strictEqual(calculateNumber(-1.6, 3.4), 1);
+  });
+
+  it("should return 2 when passed -1.4 and 3.6", function () {
+    assert.strictEqual(calculateNumber(-1.4, 3.6), 3);
+  });
+
+  it("should return 2 when passed -1.6 and 3.6", function () {
+    assert.strictEqual(calculateNumber(-1.6, 3.6), 2);
+  });
+
+  it("should return 0 when passed 0 and 0", function () {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+
+  it("should return 1 when passed 0 and 1", function () {
+    assert.strictEqual(calculateNumber(0, 1), 1);
+  });
+
+  it("should return 0 when passed 0 and 0.4", function () {
+    assert.strictEqual(calculateNumber(0, 0.4), 0);
+  });
+
+  it("should return 1 when passed 0 and 0.6", function () {
+    assert.strictEqual(calculateNumber(0, 0.6), 1);
+  });
+
+  it("should return 0 when passed 0.4 and 0", function () {
+    assert.strictEqual(calculateNumber(0.4, 0), 0);
+  });
+
+  it("should return 1 when passed 0.6 and 0", function () {
+    assert.strictEqual(calculateNumber(0.6, 0), 1);
+  });
+
+  it("should return 1 when passed 0.6 and 0.6", function () {
+    assert.strictEqual(calculateNumber(0.6, 0.6), 2);
+  });
+
+  it("should return -1 when passed -0.6 and -0.6", function () {
+    assert.strictEqual(calculateNumber(-0.6, -0.6), -2);
+  });
+
+  it("should return 0 when passed -0.4 and 0", function () {
+    assert.strictEqual(calculateNumber(-0.4, 0), 0);
+  });
+
+  it("should return 0 when passed -0.6 and 0", function () {
+    assert.strictEqual(calculateNumber(-0.6, 0), -1);
+  });
+
+  it("should return 0 when passed 0 and -0.4", function () {
+    assert.strictEqual(calculateNumber(0, -0.4), 0);
+  });
+
+  it("should return 1 when passed 0 and -0.6", function () {
+    assert.strictEqual(calculateNumber(0, -0.6), -1);
+  });
+
+  it("should return NaN when passed NaN and 1", function () {
+    assert(isNaN(calculateNumber(NaN, 1)));
+  });
+
+  it("should return NaN when passed 1 and NaN", function () {
+    assert(isNaN(calculateNumber(1, NaN)));
+  });
+
+  it("should return NaN when passed NaN and NaN", function () {
     assert(isNaN(calculateNumber(NaN, NaN)));
   });
 
-  it("should return 0 if one of the inputs is 0", () => {
-    assert.strictEqual(calculateNumber(0, 2), 2);
-    assert.strictEqual(calculateNumber(2, 0), 2);
+  it("should return 1 when passed 1 and Infinity", function () {
+    assert.strictEqual(calculateNumber(1, Infinity), Infinity);
   });
 
-  it("should return 0 if both inputs are 0", () => {
-    assert.strictEqual(calculateNumber(0, 0), 0);
+  it("should return 1 when passed Infinity and 1", function () {
+    assert.strictEqual(calculateNumber(Infinity, 1), Infinity);
+  });
+
+  it("should return 1 when passed Infinity and Infinity", function () {
+    assert.strictEqual(calculateNumber(Infinity, Infinity), Infinity);
+  });
+
+  it("should return -1 when passed -1 and -Infinity", function () {
+    assert.strictEqual(calculateNumber(-1, -Infinity), -Infinity);
+  });
+
+  it("should return -1 when passed -Infinity and -1", function () {
+    assert.strictEqual(calculateNumber(-Infinity, -1), -Infinity);
+  });
+
+  it("should return -1 when passed -Infinity and -Infinity", function () {
+    assert.strictEqual(calculateNumber(-Infinity, -Infinity), -Infinity);
   });
 });
