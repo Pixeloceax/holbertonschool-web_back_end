13e6e61acf7e38627f76f5e21e2d8c74cea043d9
Axel <axel.valentinbvs@yahoo.com>
2023-04-10 08:53:30 +0000

    add task

---
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,30 @@
+import redis from 'redis';
+const { promisify } = require('util');
+
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(value);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
